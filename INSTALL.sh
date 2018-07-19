b5285dd40aac07ab3541a184889c318b4a4ba08b
Andrew Torgesen <andrew.torgesen@gmail.com>
2018-07-19 12:29:44 -0600

    initial commit--messy md2pdf

---
+++ INSTALL.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install pandoc if not already installed
+sudo apt-get install -y pandoc
+
+# add this executable to path
+# TODO: FIND A BETTER WAY TO DO THIS
+# INSTALL_DIR="${PWD}"
+# echo 'export PATH="$PATH:'$INSTALL_DIR'"' >> ~/.bashrc
